2b9cb563eb85c045a4d2b738d5e09f448983557b
Mehul Bhange <bhangemehul@gmail.com>
2020-04-09 01:12:17 +0530

    Start

--- app/src/main/java/com/example/grocery/MainActivity.java
+++ app/src/main/java/com/example/grocery/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.grocery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i_login = new Intent(MainActivity.this,Login.class);
+                startActivity(i_login);
+                finish();
+            }
+        },3000);
+
     }
 }
